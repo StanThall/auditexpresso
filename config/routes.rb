ab31a0924c1b5fc4951040da1ad0015fd71cf28f
Adrien MENNILLO <amennillo@iMac-de-Adrien.local>
2016-11-03 14:03:09 +0100

    navlinks and new look

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :blog_posts
+  resources :tags
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'blog_posts#index'
