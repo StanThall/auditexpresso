5e0453e7accccc029d2d0aae1a42b22dc3d51dde
Adrien MENNILLO <amennillo@imac.home>
2016-11-06 21:40:30 +0100

    premier json propre

--- app/controllers/cabinets_controller.rb
+++ app/controllers/cabinets_controller.rb
@@ -1,7 +1,6 @@
 class CabinetsController < ApplicationController
 
 	def up
-		session[:return_to] ||= request.referer
 		#if session[:a_vote_up] == true
 		#	respond_to do |format|
 		#		format.js
@@ -10,30 +9,22 @@ class CabinetsController < ApplicationController
 			@cabinet = Cabinet.find(params[:id])
 			@cabinet.rating +=1
 			@cabinet.save
-			session[:a_vote_up] = true
+			#session[:a_vote_up] = true
 			respond_to do |format|
-				format.html {rtg: @cabinet.rating}
-				format.js
 				format.json {render json: @cabinet.to_json(only: :rating)}
 			end
+
 		#end
 	end
 
 	def down
-		session[:return_to] ||= request.referer
-		if session[:a_vote_down] == true
-			respond_to do |format|
-				format.js 
-			end
-		else
 			@cabinet = Cabinet.find(params[:id])
 			@cabinet.rating -=1
 			@cabinet.save
-			session[:a_vote_down] = true
+			#session[:a_vote_up] = true
 			respond_to do |format|
-				format.js
+				format.json {render json: @cabinet.to_json(only: :rating)}
 			end
-		end
 	end
 
 end
