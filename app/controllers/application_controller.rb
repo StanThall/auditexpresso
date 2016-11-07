a472d1f1954cbef65717602b6997b88e409152e8
Adrien MENNILLO <amennillo@imac.home>
2016-11-07 17:12:45 +0100

    session ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :get_session_vars
+
+#session initializers
+def get_session_vars
+	session[:a_vote_up] ||= false
+	session[:a_vote_down] ||= false
+end
+
+
 end
--- app/controllers/cabinets_controller.rb
+++ app/controllers/cabinets_controller.rb
@@ -1,30 +1,23 @@
 class CabinetsController < ApplicationController
 
 	def up
-		#if session[:a_vote_up] == true
-		#	respond_to do |format|
-		#		format.js
-		#	end
-		#else
-			@cabinet = Cabinet.find(params[:id])
-			@cabinet.rating +=1
-			@cabinet.save
-			#session[:a_vote_up] = true
-			respond_to do |format|
-				format.json {render json: @cabinet.to_json(only: :rating)}
-			end
-
-		#end
+		@cabinet = Cabinet.find(params[:id])
+		@cabinet.rating +=1
+		@cabinet.save
+		session[:a_vote_up] = @cabinet.id
+		respond_to do |format|
+			format.json {render json: @cabinet.to_json(only: :rating)}
+		end
 	end
 
 	def down
-			@cabinet = Cabinet.find(params[:id])
-			@cabinet.rating -=1
-			@cabinet.save
-			#session[:a_vote_up] = true
-			respond_to do |format|
-				format.json {render json: @cabinet.to_json(only: :rating)}
-			end
+		@cabinet = Cabinet.find(params[:id])
+		@cabinet.rating -=1
+		@cabinet.save
+		session[:a_vote_down] = @cabinet.id
+		respond_to do |format|
+			format.json {render json: @cabinet.to_json(only: :rating)}
+		end
 	end
 
 end
