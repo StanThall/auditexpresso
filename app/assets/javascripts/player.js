f502877eaf086d8a676d9b5974c3e6248b8a3f7d
Adrien MENNILLO <amennillo@pacetrader.home>
2016-11-18 16:16:41 +0100

    volume control

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -5,22 +5,30 @@ $(document).on("turbolinks:load",
 		var playBtn = document.getElementById('play-pause');
 		var muteBtn = document.getElementById('mute');
 
-		playBtn.addEventListener('click',function(){
+		video.addEventListener('click',function(){
 			if(video.paused==true){
 				video.play();
-				playBtn.innerHTML = "Pause";}
-			else {
+				$(playBtn).removeClass("fa-play");
+				$(playBtn).addClass("fa-pause");
+			} else {
 				video.pause();
-				playBtn.innerHTML = "Play";}
+				$(playBtn).removeClass("fa-pause");
+				$(playBtn).addClass("fa-play");}
 		});
 
 		muteBtn.addEventListener('click',function(){
-			if (video.muted ==false){
-				video.muted = true;
-				muteBtn.innerHTML = "Unmute";
+			if (video.volume == 1){
+				video.volume = 0.0;
+				$(muteBtn).removeClass("fa-volume-up");
+				$(muteBtn).addClass("fa-volume-off");
+			} else if (video.volume == 0.0){
+				video.volume = 0.5;
+				$(muteBtn).removeClass("fa-volume-off");
+				$(muteBtn).addClass("fa-volume-down");
 			} else {
-				video.muted = false;
-				muteBtn.innerHTML = "Mute";
+				video.volume = 1.0;
+				$(muteBtn).removeClass("fa-volume-down");
+				$(muteBtn).addClass("fa-volume-up");
 			}
 		});
 
