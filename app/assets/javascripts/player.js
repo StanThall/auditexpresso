ed435653ad4c37d3af886522a24ab146c2eca957
Adrien MENNILLO <amennillo@pacetrader.home>
2016-11-20 13:20:20 +0100

    modal fixed

--- app/assets/javascripts/player.js
+++ app/assets/javascripts/player.js
@@ -1,84 +1,83 @@
-$(document).on("turbolinks:load", 
-	function() {
-    	var video = document.getElementById('the_video');
-		var pBar = document.getElementById('progressbar');
-		var playBtn = document.getElementById('play-pause');
-		var muteBtn = document.getElementById('mute');
-		var pTextSpan = document.getElementById('progress_text');
-		var expandBtn = document.getElementById('expand');
+$(document).on('page:load',function() {
+	var video = document.getElementById('the_video');
+	var pBar = document.getElementById('progressbar');
+	var playBtn = document.getElementById('play-pause');
+	var muteBtn = document.getElementById('mute');
+	var pTextSpan = document.getElementById('progress_text');
+	var expandBtn = document.getElementById('expand');
 
-		video.addEventListener('click',function(){
-			if(video.paused==true){
-				video.play();
-				$(playBtn).fadeOut();
-				$(pTextSpan).text("C'est parti !");
-			} else {
-				video.pause();
+	video.addEventListener('click',function(){
+		if(video.paused==true){
+			video.play();
+			$(playBtn).fadeOut();
+			$(pTextSpan).text("C'est parti !");
+		} else {
+			video.pause();
+			$(playBtn).fadeIn();
+			$(playBtn).removeClass("fa-play");
+			$(playBtn).addClass("fa-pause");}
+	});
+
+	playBtn.addEventListener('click',function(){
+		if(video.paused==true){
+			video.play();
+			$(playBtn).fadeOut();
+			$(pTextSpan).text("C'est parti !");
+		} else {
+			video.pause();
+			$(playBtn).fadeIn();
+			$(playBtn).removeClass("fa-play");
+			$(playBtn).addClass("fa-pause");}
+	});
+
+	expandBtn.addEventListener('click',function(){
+		if(video.requestFullscreen){
+			video.requestFullscreen();
+		} else if(video.mozRequestFullscreen){
+			video.mozRequestFullscreen();
+		} else if (video.webkitRequestFullscreen) {
+			video.webkitRequestFullscreen();
+		}
+	});
+
+	muteBtn.addEventListener('click',function(){
+		if (video.volume == 1){
+			video.volume = 0.0;
+			$(muteBtn).removeClass("fa-volume-up");
+			$(muteBtn).addClass("fa-volume-off");
+		} else if (video.volume == 0.0){
+			video.volume = 0.5;
+			$(muteBtn).removeClass("fa-volume-off");
+			$(muteBtn).addClass("fa-volume-down");
+		} else {
+			video.volume = 1.0;
+			$(muteBtn).removeClass("fa-volume-down");
+			$(muteBtn).addClass("fa-volume-up");
+		}
+	});
+
+	video.addEventListener('timeupdate', function() {
+			var percent = Math.floor((100 / video.duration) * video.currentTime);
+			$('#progressbar').css("width",percent+"%");
+			pBar.getElementsByTagName('span')[0].innerHTML = percent+"%";
+			if (percent > 50){$(pTextSpan).text("Plus que 15 secondes !");}
+			if (percent > 60){$(pTextSpan).hide();}
+			if (percent == 100){
+				$('#progressbar').addClass("progress-bar-success");
+				pBar.getElementsByTagName('span')[0].innerHTML = "+0.10 € Merci !!!";
+				$(pBar.getElementsByTagName('span')[0]).css({
+					"font-weight" : "bold",
+					"font-size" : "1.2rem"
+				});
+				$(playBtn).removeClass();
+				$(playBtn).addClass("fa fa-thumbs-up");
+				$(playBtn).css({
+					"color" : "#FE4E00",
+					"font-size" : "20rem"
+				});
 				$(playBtn).fadeIn();
-				$(playBtn).removeClass("fa-play");
-				$(playBtn).addClass("fa-pause");}
-		});
-
-		playBtn.addEventListener('click',function(){
-			if(video.paused==true){
-				video.play();
-				$(playBtn).fadeOut();
-				$(pTextSpan).text("C'est parti !");
 			} else {
-				video.pause();
-				$(playBtn).fadeIn();
-				$(playBtn).removeClass("fa-play");
-				$(playBtn).addClass("fa-pause");}
-		});
-
-		expandBtn.addEventListener('click',function(){
-			if(video.requestFullscreen){
-				video.requestFullscreen();
-			} else if(video.mozRequestFullscreen){
-				video.mozRequestFullscreen();
-			} else if (video.webkitRequestFullscreen) {
-				video.webkitRequestFullscreen();
-			}
-		});
-
-		muteBtn.addEventListener('click',function(){
-			if (video.volume == 1){
-				video.volume = 0.0;
-				$(muteBtn).removeClass("fa-volume-up");
-				$(muteBtn).addClass("fa-volume-off");
-			} else if (video.volume == 0.0){
-				video.volume = 0.5;
-				$(muteBtn).removeClass("fa-volume-off");
-				$(muteBtn).addClass("fa-volume-down");
-			} else {
-				video.volume = 1.0;
-				$(muteBtn).removeClass("fa-volume-down");
-				$(muteBtn).addClass("fa-volume-up");
-			}
-		});
-
-		video.addEventListener('timeupdate', function() {
-	  		var percent = Math.floor((100 / video.duration) * video.currentTime);
-	  		$('#progressbar').css("width",percent+"%");
-	  		pBar.getElementsByTagName('span')[0].innerHTML = percent+"%";
-	  		if (percent > 50){$(pTextSpan).text("Plus que 15 secondes !");}
-	  		if (percent > 60){$(pTextSpan).hide();}
-	  		if (percent == 100){
-	  			$('#progressbar').addClass("progress-bar-success");
-	  			pBar.getElementsByTagName('span')[0].innerHTML = "+0.10 € Merci !!!";
-	  			$(pBar.getElementsByTagName('span')[0]).css({
-	  				"font-weight" : "bold",
-	  				"font-size" : "1.2rem"
-	  			});
-	  			$(playBtn).removeClass();
-	  			$(playBtn).addClass("fa fa-thumbs-up");
-	  			$(playBtn).css({
-	  				"color" : "#FE4E00",
-	  				"font-size" : "20rem"
-	  			});
-	  			$(playBtn).fadeIn();
-	  		} else {
-	  			$('#progressbar').removeClass("progress-bar-success");};
-			}, false);
-  	});
+				$('#progressbar').removeClass("progress-bar-success");};
+		}, false);
+});
 
