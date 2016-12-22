1bab1a684dbf7a0923bf990f0bd6f7d39f9cb264
Adrien MENNILLO <amennillo@pacetrader.local>
2016-12-22 13:55:13 +0100

    page in log_mailer

--- app/mailers/logs_mailer.rb
+++ app/mailers/logs_mailer.rb
@@ -1,7 +1,8 @@
 class LogsMailer < ApplicationMailer
 	default from: "contact@auditexpresso.net"
 
-	def video_finish
+	def video_finish(page)
+		@page = page
 		mail to: "adrien.mennillo@gmail.com", subject: "Une vidéo vient d'être vue ! "
 	end
 end
