7e3779092815925e54454b35f9c87ecc3a49fad4
Adrien MENNILLO <amennillo@pacetrader.home>
2017-01-08 10:10:33 +0100

    mobile button text

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -13,18 +13,17 @@ class LogsController < ApplicationController
     
     #LogsMailer.action_taken(@log.action_id, @log.page, @log.agent, @log.session_id).deliver_now if @log.action_id == 3 || @log.action_id == 6 || @log.action_id == 7
 
-    info = " /sur: " + @log.page + " /par: " + @log.session_id 
-    info2 = " /sur: " + @log.page + " /par: " + @log.session_id + " /device: " + @log.agent
+    info = " \n/ *sur:* " + @log.page + " / *par:* " + @log.session_id + " / *device:* " + @log.agent
 
     client = Slack::Web::Client.new(user_agent: 'Slack Ruby Client/1.0')
 
     case @log.action_id
     when 3
-      client.chat_postMessage(channel: '#auditexpresso_logs', text: '*Video Vue !*\n' + info2, as_user: true)
+      client.chat_postMessage(channel: '#auditexpresso_logs', text: ':+1: *Video Vue !*' + info, as_user: true)
     when 6
-      client.chat_postMessage(channel: '#auditexpresso_logs', text: '*Popup !*\n'+ info, as_user: true)
+      client.chat_postMessage(channel: '#auditexpresso_logs', text: '*Popup !*', as_user: true)
     when 7
-      client.chat_postMessage(channel: '#auditexpresso_logs', text: '*Popup Fermée !*\n'+ info, as_user: true)
+      client.chat_postMessage(channel: '#auditexpresso_logs', text: '*Popup Fermée !*', as_user: true)
     end
 
   end
