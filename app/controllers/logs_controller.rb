09af22c43a2dc4b34fc8f0d294c2d4692b5162a9
Adrien MENNILLO <amennillo@pacetrader.home>
2017-01-08 22:32:43 +0100

    modifs

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -20,6 +20,8 @@ class LogsController < ApplicationController
     case @log.action_id
     when 3
       client.chat_postMessage(channel: '#auditexpresso_logs', text: ':+1: *Video Vue !*' + info, as_user: true)
+    when 2 
+      client.chat_postMessage(channel: '#auditexpresso_logs', text: '*Button Click*' + info, as_user: true)
     end
 
   end
